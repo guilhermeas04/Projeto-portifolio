645820250e0e1c3e5f45102f4c4628be0b3024ae
Guilherme Lana <contato.guilhermelana@gmail.com>
2025-08-20 20:16:15 -0300

    feat: implement language context and enhance UI with fade-in effects

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -1,23 +1,26 @@
-import './App.css';
-import About from './sections/About/About';
-import Contact from './sections/Contact/Contact';
-import Experience from './sections/Experience/Experience';
-import Header from './sections/Header/Header';
-import Hero from './sections/Hero/Hero';
-import Projects from './sections/Projects/Projects';
+import "./App.css";
+import About from "./sections/About/About";
+import Contact from "./sections/Contact/Contact";
+import Experience from "./sections/Experience/Experience";
+import Header from "./sections/Header/Header";
+import Hero from "./sections/Hero/Hero";
+import Projects from "./sections/Projects/Projects";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Projects />
-        <Experience />
-      </main>
-      <Contact />
-    </div>
+    <LanguageProvider>
+      <div className="App">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Projects />
+          <Experience />
+        </main>
+        <Contact />
+      </div>
+    </LanguageProvider>
   );
 }
 
